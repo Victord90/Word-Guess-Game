40c53d535150bb80c8db0ea64ae7b13089a75f3d
Victor Dominguez <vd071090@gmail.com>
2019-10-05 21:46:58 -0400

    added working functions to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,74 @@
 var answer = ["zelda", "pac-man", "mario", "star-fox", "kirby", "sonic","mega-man", "donkey-kong", "spyro"];
 var wins = 0;
 var losses = 0;
-var maxNumberGuesses = 10;
+var maxGuesses = 10;
 var lettersGuessed = [];
 var answerArray = [];
 var finished = false;
-
-// console.log(answer.length);
-
-
-// var directionText = document.getElementById("direction-text");
-// var userChoiceText = document.getElementById("userchoice-text");
-// var winsText = document.getElementById("wins-text");
-// var lossText = document.getElementById("loss-text");
-
-document.onkeyup = function() {
+var computerChoice = [];
+var guessesRemaining = 0;
+var letterRemaining = [];
 
 
-    var userGuess = event.key;
-        // console.log(userGuess);
-
-    var computerChoice = answer[Math.floor(Math.random() * answer.length)];
-
-        if (userGuess.includes(answer)) {
-
-        if (userGuess.includes(answer)) {
-            userChoiceText++;
-        }else {
-            lossText++;
-        }
-
-        directionText.textContent = "";
+    function start () {
 
 
-        userChoiceText.textContent = "You chose: " + userGuess;
-        computerChoiceText.textContent = "The computer Chose: " + computerChoice;
-        winsText.textContent = "wins: " + wins;
-        lossesText.textContent = "losses: " + losses;
+    computerChoice = answer[Math.floor(Math.random() * answer.length)];
 
+
+    answerArray = [];
+      
+
+
+    for (var i = 0; i < computerChoice.length; i++) {
+        answerArray = [i] = "_";
+        
     }
 
-    
+    letterRemaining = answer.length;
     
 
 
+
+     guessesRemaining = maxGuesses;
+     lettersGuessed = [];
+     console.log(answer.length)
+     
+     
+    update()
 };
+
+
+
+
+    function update () {
+
+
+
+         var directionText = document.getElementById("direction-text");
+         var userChoiceText = document.getElementById("userchoice-text");
+         var winsText = document.getElementById("wins-text");
+         var lossText = document.getElementById("loss-text");
+
+
+
+         directionText.textContent = "";
+
+
+         userChoiceText.textContent = "You chose: " + userGuess;
+         computerChoiceText.textContent = "The computer Chose: " + computerChoice;
+         winsText.textContent = "wins: " + wins;
+         lossesText.textContent = "losses: " + losses;
+
+         
+        
+
+     };
+
+    
+    
+   
+
 
 
 
